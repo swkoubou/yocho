44ba50dcaf1bfa05ed4f60d2f419113343027f9c
KessaPassa <k.someya.gyao@gmail.com>
2016-08-06 18:12:45 +0900

    make.phpほぼ完成

--- make.php
+++ make.php
@@ -3,30 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <title>予定を合わせて帳っ!!</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
-</body>
-</html>
-
 <?php
-echo '<form method="post" action="">';
+session_start();
+include 'ChromePhp.php';
+echo '<form method="post" action="" style="display: inline">';
 echo '<br/>';
 
 echo 'イベント名  <input name="eventname" type="text"><br/><br/>';
 echo '<br/>';
 
-echo "<select name='month'>";
-for ($i = 1; $i <= 12; $i++) {
-    echo "<option>".$i;
-}
-echo "</select> 月　";
+echo '日にち  <input class="date" name="date0" type="text"><br/>';
 
-echo "<select name='day'>";
-for ($i = 1; $i <= 31; $i++) {
-    echo "<option>".$i;
-}
-echo "</select> 日　";
+echo '<br/><br/><br/>';
+echo '<button id="decide" style="width:50px">決定</button>';
+echo '</form>';
 
-echo '<br/>';
-echo '</form>'
+echo '<span>ㅤㅤ</span>'; //空白文字を入れていい感じにボタンの間をあける
+echo '<button id="add" style="width:50px">add</button>';
 ?>
+</body>
+</html>
+
+<script>
+    $(function(){
+        var i=0;
+        $('#add').click(function(){
+            i++;
+            $('.date:last').after('<br/><br/>日にち  <input class="date" name="data'+ i +'" type="text">');
+        });
+    });
+</script>

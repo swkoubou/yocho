e8a8e30acf4bdaa4db3ff26292db4084ddee7bbc
tibimosu <tibimosu1128@gmail.com>
2016-08-06 17:33:15 +0900

    GETとPOSTの使い分け処理の追加

--- api/registration.php
+++ api/registration.php
@@ -4,22 +4,26 @@ require __DIR__.'/../php/db.php';
 
 const TABLE = 'ivent';
 
-DB::connectDb();
-$data = DB::getTable(TABLE);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  DB::connectDb();
+  $data = DB::getTable(TABLE);
 
-$res = [];
-foreach ($data as $datum) {
-  $ps = explode(',', $datum['participants']);
-  $ds = explode(',', $datum['dates']);
-  $res[] = [
-    'ivent_id' => $datum['ivent_id'],
-    'ivent_name' => $datum['ivent_name'],
-    'participants' => $ps,
-    'dates' => $ds,
-    'url' => $datum['url'],
-    'deadline' => $datum['deadline']
-  ];
+  $res = [];
+  foreach ($data as $datum) {
+    $ps = explode(',', $datum['participants']);
+    $ds = explode(',', $datum['dates']);
+    $res[] = [
+      'ivent_id' => $datum['ivent_id'],
+      'ivent_name' => $datum['ivent_name'],
+      'participants' => $ps,
+      'dates' => $ds,
+      'url' => $datum['url'],
+      'deadline' => $datum['deadline']
+    ];
+  }
+
+  header("Content-Type: application/json");
+  echo json_encode($res);
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
 }
-
-header("Content-Type: application/json");
-echo json_encode($res);
